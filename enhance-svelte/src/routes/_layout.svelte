7311badbf13e8e003cecc1cc73e229e4deec5afd
DavidJonesADA <54673427+DavidJonesADA@users.noreply.github.com>
2021-05-17 12:42:04 +0100

    Begun layout on index, added main images

---
+++ enhance-svelte/src/routes/_layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+</script>
+
+<style>
+
+@font-face {
+    font-family: Ubuntu;
+    src: url('../fonts/Ubuntu.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+    :global(body){
+        background-color: white;
+        background-image: url("../images/background.png");
+		width: 100vw;
+		height: 100vh;
+		background-repeat:no-repeat;
+		background-size:cover;
+		font-family: Ubuntu;
+		
+    }
+</style>
+
+<main>
+	<slot></slot>
+</main>
---
+++ enhance-svelte/src/routes/index.svelte
@@ -0,0 +1,52 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<div style="display: inline-flex; justify-content: center; align-items: center; width: 100%; flex-direction: column;">
+	<img src="../images/trello.svg" alt="trello" style="width: 4rem; margin: 1rem; margin-bottom: -0.75rem; align-self: flex-start; ">
+	<p style="font-size: 3em; margin: 0; margin-left: auto; margin-right: 1rem">Enhance</p>
+	<img src="../images/github.svg" alt="github" style="width: 4rem; align-self: flex-end;">
+
+
+</div>
+
+
+
